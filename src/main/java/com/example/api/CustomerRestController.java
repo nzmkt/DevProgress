6b5bac7a9e74781e73907b2f92e0d0d957685ebb
makoto <makoto.fy39w88xs.33@gmail.com>
2017-09-02 13:02:03 +0900

    顧客管理システム作成

--- src/main/java/com/example/api/CustomerRestController.java
+++ src/main/java/com/example/api/CustomerRestController.java
@@ -2,11 +2,9 @@
 package com.example.api;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,8 +28,8 @@ public class CustomerRestController {
     CustomerService customerService;
 
     @GetMapping
-    Page<Customer> getCustomers(@PageableDefault Pageable pageable) {
-        Page<Customer> customers = customerService.findAll(pageable);
+    List<Customer> getCustomers() {
+        List<Customer> customers = customerService.findAll();
         return customers;
     }
 
--- src/main/java/com/example/repository/CustomerRepository.java
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -1,12 +1,13 @@
 package com.example.repository;
 
-import com.example.domain.Customer;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import com.example.domain.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
